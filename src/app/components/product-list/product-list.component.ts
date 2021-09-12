c7caa72cbe0573cd88dacc9ba84d32648d11e7ce
Tianyi Zhou <zcarol.530@gmail.com>
2021-09-12 01:05:24 -0400

    enhanced ProductListComponent to search for products with ProductService

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,11 +12,11 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
-
   currentCategoryId: number;
+  searchMode: boolean;
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute){}
+    private route: ActivatedRoute) { }
 
 
   ngOnInit(): void {
@@ -26,12 +26,33 @@ export class ProductListComponent implements OnInit {
     this.listProducts();
   }
 
-  listProducts(){
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
+    if (this.searchMode){
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+  }
+
+
+  handleSearchProducts() {
+    const theKeyWord: string = this.route.snapshot.paramMap.get('keyyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyWord).subscribe(
+      data => {
+        this.products =data;
+      }
+    );
+  }
+
+  handleListProducts() {
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-    if (hasCategoryId){
+    if (hasCategoryId) {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     } else {
